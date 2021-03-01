f10eecb8eae35cbe25a5adf51815d30ff234befc
whatAnswer <554528737@qq.com>
2021-03-01 16:24:12 +0800

    页面添加

--- vue-rem/src/views/Home.vue
+++ vue-rem/src/views/Home.vue
@@ -2,11 +2,41 @@
   <div class="home">
     <div>
       <div class="topbac">
-        <div><img src="../assets/logo.png" alt="" class="logo"></div>
-        <div class="banner"></div>
-        <div></div>
+        <div class="logo"><img src="../assets/logo.png" alt=""></div>
+        <div class="box">
+          
+        </div>
       </div>
-      <div class="bottombac"></div>
+      <div class="bottombac">
+        <div class="frp">
+          <div class="fr">
+
+          </div>
+          <div class="fr2">
+
+          </div>
+          <div class="namelist">
+            <div class="tittle">
+              <div class="medal"><img src="../assets/medal.png" alt=""></div>
+              <div class="medaltittle">中奖名单</div>
+            </div>
+            <div>
+              <div class="prizeline">
+                <div class="name">放飞风筝</div>
+                <div class="prize">15分钟前抽中果汁机</div>
+              </div>
+              <div class="prizeline">
+                <div class="name">千小库的家-030...</div>
+                <div class="prize">22分钟前抽中XL蒸锅</div>
+              </div>
+              <div class="prizeline">
+                <div class="name">深圳贺康</div>
+                <div class="prize">30分钟前抽中果汁机</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,15 +58,131 @@ export default {
     height: 1285px;
     background-image: url("../assets/topbac.jpg");
     background-size: 100% 100%;
+
+    .logo{
+      text-align: center;
+
+      img{
+        width: 325px;
+        display: block;
+        margin: auto;;
+      }
+    }
+
+    .box{
+      height: 1020px;
+      margin-top: 274px;
+    }
   }
   .bottombac{
     width: 100%;
     height: 1285px;
     background-image: url("../assets/bottombac.jpg");
     background-size: 100% 100%;
-  }
-  .logo{
-    width: 325px;
+
+    .frp{
+      padding-top: 148px;  
+
+      .fr{
+        width: 658px;
+        height: 341px;
+        background-image: url("../assets/frame.png");
+        background-size: 100% 100%;
+        margin: auto;
+      }
+
+      .fr2{
+        width: 658px;
+        height: 341px;
+        background-image: url("../assets/frame.png");
+        background-size: 100% 100%;
+        margin: auto;
+        margin-top: 18px;
+      }
+
+      .namelist{
+        width: 658px;
+        height: 336px;
+        background-image: url("../assets/namelist.png");
+        background-size: 100% 100%;
+        margin: auto;
+        margin-top: 35px;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        padding: 0 66px;
+        padding-top: 82px;
+
+        .tittle{
+          font-size: 0;
+          margin-bottom: 31.5875px;
+        }
+
+        .medal{
+          display: inline-block;
+          vertical-align: middle;
+
+          img{
+            display: block;
+            width: 25px;
+            height: 32px;
+          }
+        }
+
+        .medaltittle{
+          display: inline-block;
+          vertical-align: middle;
+          font-size: 25.56px;
+          color: #a64122;
+          font-weight: bold;
+          margin-left: 21px;
+        }
+
+        .prizeline{
+          line-height: 1.5;
+          margin-top: 8.825px;
+          display: box;
+          display: -webkit-box;
+          display: -moz-box;
+          display: -ms-flexbox;
+          display: -webkit-flex;
+          display: flex;
+          -webkit-box-align: center;
+          -webkit-align-items: center;
+          -moz-align-items: center;
+          -ms-align-items: center;
+          -o-align-items: center;
+          align-items: center;
+          align-content: center;
+          -moz-box-pack: center;
+          box-pack: justify;
+          -moz-box-pack: justify;
+          -webkit-box-pack: justify;
+          -webkit-justify-content: space-between;
+          -moz-justify-content: space-between;
+          -ms-justify-content: space-between;
+          -o-justify-content: space-between;
+          justify-content: space-between;
+        }
+
+        .name{
+          font-size: 22.35px;
+          color: #d84b26;
+          font-weight: bold;
+          width: 188px;
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          text-align: left;
+        }
+
+        .prize{
+          font-size: 22.35px;
+          color: #d84b26;
+          font-weight: bold;
+        }
+      }
+    }
   }
 </style>
 
